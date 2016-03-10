07dea92f8f57c6c482bd2e37d4889b34f0771d81
Hanna Lindgren <han.lind@gmail.com>
2016-03-10 17:42:36 +0100

    symbolset : go docs

--- symbolset/structs.go
+++ symbolset/structs.go
@@ -107,7 +107,7 @@ func (m Mapper) mapTranscription(input string) (string, error) {
 	return m.postFilter(res, m.to)
 }
 
-// SymbolPair is a tuple inside the Mapper
+// SymbolPair is a tuple inside the Mapper used to store the symbol mappings in a list in preserved order
 type SymbolPair struct {
 	Sym1 Symbol
 	Sym2 Symbol
