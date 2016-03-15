d06829ef1b3d9c484842aa21091915a4d83d3ca6
Hanna Lindgren <han.lind@gmail.com>
2016-03-15 16:39:55 +0100

    line : misc

--- line/line_test.go
+++ line/line_test.go
@@ -1,20 +1,21 @@
 package line
 
 import (
+	"fmt"
 	"testing"
 )
 
 var fsExpField = "For field %v, expected: '%v' got: '%v'"
-var fsExpFields = "Expected: '%v' got: '%v'"
+var fsExp = "Expected: '%v' got: '%v'"
 
 func checkResult(t *testing.T, expect map[Field]string, result map[Field]string) {
 	if len(expect) != len(result) {
-		t.Errorf(fsExpFields, expect, result)
+		t.Errorf(fsExp, expect, result)
 	} else {
 		for f, ex := range expect {
 			re := result[f]
 			if re != ex {
-				fn, err := FieldName(f)
+				fn, err := f.Name()
 				if err != nil {
 					t.Errorf("didn't expect error here : %v", err)
 				}
@@ -24,7 +25,7 @@ func checkResult(t *testing.T, expect map[Field]string, result map[Field]string)
 	}
 }
 
-func Test_1(t *testing.T) {
+func Test_Parse_01(t *testing.T) {
 	var fs = map[Field]int{
 		Orth:       0,
 		Pos:        1,
@@ -35,9 +36,9 @@ func Test_1(t *testing.T) {
 		Translang1: 14,
 		Lemma:      15,
 	}
-	fmt := Format{"\t", fs}
+	fmt := Format{"\t", fs, 16}
 
-	input := "hannas	PM	GEN	hannas	-	-	swe	-	-	-	-	\"\" h a . n a s	-	-	swe	hanna_01	-	-"
+	input := "hannas	PM	GEN	hannas	-	-	swe	-	-	-	-	\"\" h a . n a s	-	-	swe	hanna_01"
 
 	var expect = map[Field]string{
 		Orth:       "hannas",
@@ -55,5 +56,76 @@ func Test_1(t *testing.T) {
 	} else {
 		checkResult(t, expect, result)
 	}
+
+}
+
+func Test_Parse_02(t *testing.T) {
+	var fs = map[Field]int{
+		Orth:       0,
+		Pos:        1,
+		Morph:      2,
+		Decomp:     3,
+		WordLang:   6,
+		Trans1:     11,
+		Translang1: 14,
+		Lemma:      15,
+	}
+	fmt := Format{"\t", fs, 16}
+
+	input := "hannas	PM	GEN	hannas	-	-	swe	-	-	-	-	\"\" h a . n a s	-	-	swe	hanna_01	-	-"
+
+	var _, err = fmt.Parse(input)
+	if err == nil {
+		t.Errorf("Expected error here")
+	}
+
+}
+
+func Test_String_01(t *testing.T) {
+	var fs = map[Field]int{
+		Orth:       0,
+		Pos:        1,
+		Morph:      2,
+		Decomp:     3,
+		WordLang:   6,
+		Trans1:     11,
+		Translang1: 14,
+		Lemma:      15,
+	}
+	fmt := Format{";", fs, 16}
+
+	expect := "hannas;PM;GEN;hannas;;;eng;;;;;\"\" h a . n a s;;;swe;hanna_01"
+
+	var input = map[Field]string{
+		Orth:       "hannas",
+		Pos:        "PM",
+		Morph:      "GEN",
+		Decomp:     "hannas",
+		WordLang:   "eng",
+		Trans1:     "\"\" h a . n a s",
+		Translang1: "swe",
+		Lemma:      "hanna_01",
+	}
+	var result, err = fmt.String(input)
+	if err != nil {
+		t.Errorf("didn't expect error here : %v", err)
+	} else if result != expect {
+		t.Errorf(fsExp, expect, result)
+	}
+
+}
+
+func Test_FieldName(t *testing.T) {
+	var result, expect string
+	var err error
+
+	result, err = Orth.Name()
+	expect = "Orth"
+	if err != nil {
+		t.Errorf("didn't expect error here : %v", err)
+	} else if result != expect {
+		t.Errorf(fsExp, "Orth", result)
+	}
+	fmt.Println(result)
 
 }
