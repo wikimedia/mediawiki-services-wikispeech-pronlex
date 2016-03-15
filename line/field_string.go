22a1e8df9172775b5f46c140328e542e5f5018bb
Hanna Lindgren <han.lind@gmail.com>
2016-03-15 16:49:06 +0100

    line : misc

--- line/field_string.go
+++ line/field_string.go
@@ -8,7 +8,7 @@ const _Field_name = "OrthPosMorphDecompWordLangTrans1Translang1Trans2Translang2T
 
 var _Field_index = [...]uint8{0, 4, 7, 12, 18, 26, 32, 42, 48, 58, 64, 74, 80, 90, 96, 106, 112, 122, 127, 141}
 
-func (i Field) string() string {
+func (i Field) String() string {
 	if i < 0 || i >= Field(len(_Field_index)-1) {
 		return fmt.Sprintf("Field(%d)", i)
 	}
