bcb829d9669ef734855eaa894709f1482ff52aa3
Hanna Lindgren <han.lind@gmail.com>
2016-03-24 12:44:22 +0100

    pronlex validation : initial commit

---
+++ validation/validation.go
@@ -0,0 +1,36 @@
+package validation
+
+import "github.com/stts-se/pronlex/dbapi"
+
+/*
+Result is a validation result with an arbitrary
+	Name - arbitrary string
+	Level - typically indicating severity (e.g. Info/Warning/Fatal/Format)
+	Message - arbitrary string
+*/
+type Result struct {
+	Name    string
+	Level   string
+	Message string
+}
+
+// Rule interface. To create a validation.Rule, make a struct implementing Validate(dbapi.Entry) []Result
+type Rule interface {
+	Validate(dbapi.Entry) []Result
+}
+
+// Validator is a struct containing a slice of rules
+type Validator struct {
+	Rules []Rule
+}
+
+// Validate is used to validate an entry using a slice of rules
+func (v Validator) Validate(e dbapi.Entry) []Result {
+	var result []Result
+	for _, rule := range v.Rules {
+		for _, res := range rule.Validate(e) {
+			result = append(result, res)
+		}
+	}
+	return result
+}
---
+++ validation/validation_test.go
@@ -0,0 +1,10 @@
+package validation
+
+import "testing"
+
+//var fsExp = "Expected: '%v' got: '%v'"
+
+func Test1(t *testing.T) {
+	// HL TODO
+	//t.Errorf(fsExp, expect, result)
+}
