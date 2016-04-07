d30754bd235351b0f08da4e8f1e8b9c1399d4f3f
Hanna Lindgren <han.lind@gmail.com>
2016-04-07 12:21:29 +0200

    config

--- config/config.go
+++ config/config.go
@@ -11,26 +11,32 @@ import (
 
 // JSON representation of a configuration used with a lexicon (string values only)
 type JSON struct {
-	Lexicons    []string `json:"lexicons"`    // lexicon name(s) in lex db
-	StatusNames []string `json:"statusNames"` // should be in lex db
-	PosTags     []string `json:"posTags"`     // should be in lex db
-	Languages   []string `json:"languages"`   // should be in lex db
-	Encoding    string   `json:"encoding"`    // should be in lex db
-	OrthCharsRe string   `json:"orthCharsRe"` // should be in lex db
-	Validator   string   `json:"validator"`   // should be in lex db?
-	LineParsers []string `json:"lineParsers"` // should NOT be in lex db
+	Lexicons    []string `json:"lexicons"`
+	StatusNames []string `json:"statusNames"`
+	PosTags     []string `json:"posTags"`
+	Languages   []string `json:"languages"`
+	Users       []string `json:"users"`
+	Sources     []string `json:"string"`
+	Encoding    string   `json:"encoding"`
+	OrthCharsRe string   `json:"orthCharsRe"`
+	Validator   string   `json:"validator"`
+	LineParsers []string `json:"lineParsers"`
+	TTSMapper   string   `json:"ttsMapper"`
 }
 
 // Config configuration used with a lexicon
 type Config struct {
-	Lexicons    []string             // lexicon name(s) in lex db
-	StatusNames []string             // should be in lex db
-	PosTags     []string             // should be in lex db
-	Languages   []string             // should be in lex db
-	Encoding    string               // should be in lex db
-	OrthCharsRe string               // should be in lex db
-	Validator   validation.Validator // should be in lex db?
-	LineParsers []line.Parser        // should NOT be in lex db
+	Lexicons    []string             // lexicon name(s) in some db
+	StatusNames []string             // should be in some (the lex?) db
+	PosTags     []string             // should be in some (the lex?) db
+	Languages   []string             // should be in some (the lex?) db
+	Users       []string             // should be in some (the lex?) db
+	Sources     []string             // should be in some (the lex?) db
+	Encoding    string               // should be in some (the lex?) db
+	OrthCharsRe string               // should be in some (the lex?) db
+	Validator   validation.Validator // should be in some (the lex?) db?
+	LineParsers []line.Parser        // should be in some (the lex?) db?
+	TTSMapper   string               //??
 }
 
 // New creates a new Config instance from a JSON representation
@@ -50,16 +56,23 @@ func New(json JSON) (Config, error) {
 		StatusNames: json.StatusNames,
 		PosTags:     json.PosTags,
 		Languages:   json.Languages,
+		Users:       json.Users,
+		Sources:     json.Sources,
 		Encoding:    json.Encoding,
 		OrthCharsRe: json.OrthCharsRe,
 		Validator:   v,
 		LineParsers: parsers,
+		TTSMapper:   json.TTSMapper,
 	}
 	return result, nil
 }
 
 func validatorForName(name string) (validation.Validator, error) {
-	if v, ok := validators()[name]; ok {
+	validators, err := validators()
+	if err != nil {
+		return validation.Validator{}, err
+	}
+	if v, ok := validators[name]; ok {
 		return v, nil
 	}
 	return validation.Validator{}, fmt.Errorf("No validator for name: %s", name)
@@ -77,10 +90,14 @@ func lineParsersForNames(names []string) ([]line.Parser, error) {
 	return result, nil
 }
 
-func validators() map[string]validation.Validator {
-	return map[string]validation.Validator{
-		"NstDemoValidator": validator.NewNSTDemoValidator(),
+func validators() (map[string]validation.Validator, error) {
+	nstDemo, err := validation.NewNSTDemoValidator()
+	if err != nil {
+		return nil, err
 	}
+	return map[string]validation.Validator{
+		"NstDemoValidator": nstDemo,
+	}, nil
 
 }
 func lineParser(name string) (line.Parser, error) {
