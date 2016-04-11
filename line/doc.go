33a1562be4a545f830bf941f94659db6ca5eae62
Hanna Lindgren <han.lind@gmail.com>
2016-04-11 11:58:05 +0200

    line doc

--- line/doc.go
+++ line/doc.go
@@ -3,5 +3,7 @@
 Interfaces:
 * Format - simple line format definition (field names and indices)
 * Parser - a more complex parser, containing a Format definition, but also adds the possibility to write specific code for parsing that cannot be handeled by the Format specs alone (multi-value fields, etc).
+
 */
+
 package line
