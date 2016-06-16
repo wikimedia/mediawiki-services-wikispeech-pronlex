32e04f5003c0225804699acb878aa4f61b793098
Nikolaj Lindberg <nikolaj@stts.se>
2016-06-16 16:35:32 +0200

    added indices to foreign keys, speeding up delete as well as select

--- dbapi/schema.go
+++ dbapi/schema.go
@@ -17,6 +17,7 @@ CREATE TABLE Symbolset (
     ipa varchar(128)
   );
 CREATE INDEX idx37380686 on Symbolset (symbol);
+CREATE UNIQUE INDEX idx8bc90a52 on Symbolset (lexiconId,symbol);
 CREATE TABLE Lemma (
     reading varchar(128) not null,
     id integer not null primary key autoincrement,
@@ -26,6 +27,7 @@ CREATE TABLE Lemma (
 CREATE INDEX idx21d604f4 on Lemma (reading);
 CREATE INDEX idx273f055f on Lemma (paradigm);
 CREATE INDEX idx149303e1 on Lemma (strn);
+CREATE UNIQUE INDEX idx407206e8 on Lemma (strn,reading);
 CREATE TABLE SurfaceForm (
     id integer not null primary key autoincrement,
     strn varchar(128) not null
@@ -42,6 +44,8 @@ CREATE TABLE Entry (
 foreign key (lexiconId) references Lexicon(id));
 CREATE INDEX idx28d70584 on Entry (language);
 CREATE INDEX idx15890407 on Entry (strn);
+CREATE INDEX entrylexid ON Entry (lexiconId);
+CREATE INDEX idx4a250778 on Entry (strn,language);
 CREATE TABLE EntryValidation (
     id integer not null primary key autoincrement,
     entryid integer not null,
@@ -52,6 +56,7 @@ CREATE TABLE EntryValidation (
     foreign key (entryId) references Entry(id) on delete cascade);
 CREATE INDEX evallev ON EntryValidation(level);
 CREATE INDEX evalnam ON EntryValidation(name);
+CREATE INDEX entvalEid ON EntryValidation(entryId); 
 CREATE TABLE EntryStatus (
     name varchar(128) not null,
     source varchar(128) not null,
@@ -64,6 +69,7 @@ CREATE TABLE EntryStatus (
 CREATE INDEX esn ON EntryStatus (name);
 CREATE INDEX ess ON EntryStatus (source);
 CREATE INDEX esc ON EntryStatus (current);
+CREATE INDEX esceid ON EntryStatus (entryId);
 CREATE UNIQUE INDEX eseii ON EntryStatus  (entryId, id);
 CREATE TABLE Transcription (
     entryId integer not null,
@@ -75,6 +81,8 @@ CREATE TABLE Transcription (
     strn varchar(128) not null,
     sources TEXT not null,
 foreign key (entryId) references Entry(id) on delete cascade);
+CREATE INDEX traeid ON Transcription (entryId);
+
 CREATE TABLE TranscriptionStatus (
     name varchar(128) not null,
     source varchar(128) not null,
@@ -82,20 +90,19 @@ CREATE TABLE TranscriptionStatus (
     transcriptionId integer not null,
     id integer not null primary key autoincrement,
 foreign key (transcriptionId) references Transcription(id) on delete cascade);
+CREATE INDEX nizze ON TranscriptionStatus (transcriptionId); 
 CREATE TABLE Lemma2Entry (
     entryId bigint not null,
     lemmaId bigint not null,
 unique(lemmaId,entryId),
 foreign key (entryId) references Entry(id) on delete cascade, -- NL20160226, 20160614
 foreign key (lemmaId) references Lemma(id) on delete cascade); -- NL20160226, 20160614
-CREATE INDEX l2eind on Lemma2Entry (lemmaId); -- NL20160309 (meaningless line?)
+CREATE INDEX l2eind1 on Lemma2Entry (entryId); -- NL20160309 (meaningless line?)
+CREATE INDEX l2eind2 on Lemma2Entry (lemmaId); -- NL20160309 (meaningless line?)
 CREATE UNIQUE INDEX l2euind on Lemma2Entry (lemmaId,entryId); -- NL20160309
 CREATE UNIQUE INDEX idx46cf073d on Lemma2Entry (entryId);
-CREATE TABLE SurfaceForm2Entry (
-    entryId bigint not null,
-    surfaceFormId bigint not null,
-unique(surfaceFormId,entryId));
-CREATE UNIQUE INDEX idx8bc90a52 on Symbolset (lexiconId,symbol);
-CREATE INDEX idx4a250778 on Entry (strn,language);
-CREATE UNIQUE INDEX idx407206e8 on Lemma (strn,reading);
+-- CREATE TABLE SurfaceForm2Entry (
+--    entryId bigint not null,
+--    surfaceFormId bigint not null,
+-- unique(surfaceFormId,entryId));
 `
