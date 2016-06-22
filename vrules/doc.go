5f7628e8bb8cf2961b46d2200dccf21e5511d034
Hanna Lindgren <han.lind@gmail.com>
2016-06-22 12:11:43 +0200

    r

--- vrules/doc.go
+++ vrules/doc.go
@@ -1,2 +1,2 @@
-// Package vrules contains a few general validation rule types. The validation design and interfaces can be found in package validation
+// Package vrules contains a few general validation rule types. The validation design and interfaces can be found in package validation.
 package vrules
