9d273208bd72329f59267d80d86a7bf005d8ce57
Nikolaj Lindberg <nikolaj@stts.se>
2016-11-28 13:35:17 +0100

    fumbling about

--- decompounder/server/src/decomp_demo.ts
+++ decompounder/server/src/decomp_demo.ts
@@ -16,15 +16,28 @@ const baseURL: string = window.location.origin;
 
 class NIZZE {
 
-    decomps: KnockoutObservable<string>
+    word: KnockoutObservable<string>;
+
+    zmurf: KnockoutComputed<string>;
+
+    decomps: KnockoutObservable<Decomp[]>;
 
     constructor() {
-        this.decomps = <KnockoutObservable<string>>ko.observable();
+        this.word = <KnockoutObservable<string>>ko.observable("skrotbil");
+        this.decomps = <KnockoutObservable<Decomp[]>>ko.observable([]);
+        this.zmurf = ko.computed({
+            read: () => {
+
+                this.decomp0(baseURL, this.word())
+                return "";//this.word().toUpperCase();
+            }
+        });
+        //this.word("bilskrot");
     }
 
-    decomp0(baseURL: string, word: string): void { //Decomp[] {
+    decomp0(baseURL: string, word: string): string { //Decomp[] {
         let itself = this;
-        let res = [{ parts: [] }];
+        let res: string = "";// [{ parts: [] }];
 
         console.log("BAseURL: " + baseURL);
         let url = baseURL + "/decomp/decomp?word=" + word
@@ -38,7 +51,10 @@ class NIZZE {
             if (r.status === 200) {
 
                 console.log(r.responseText);
-                itself.decomps(r.responseText);
+                //let d: string[] = [r.responseText];
+                let d: Decomp[] = <Decomp[]>JSON.parse(r.responseText);
+                itself.decomps(d);
+                res = r.responseText
 
             } else {
                 console.log("readyState: " + r.readyState);
@@ -49,7 +65,7 @@ class NIZZE {
         }
 
         r.send();
-
+        return res;
     }
 
     decomp(): void {
