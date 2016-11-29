02ec4805c6211b533d25a5144bf7549362130f09
Hanna Lindgren <han.lind@gmail.com>
2016-11-29 15:57:03 +0100

    search_demo with ipa mapping

--- web/lexsearch/src/search_demo.ts
+++ web/lexsearch/src/search_demo.ts
@@ -1,6 +1,6 @@
 class Lexicon {
     name: string
-    symbolset: string
+    symbolSetName: string
 }
 
 class SearchModel {
@@ -8,15 +8,15 @@ class SearchModel {
     selectedLexicon: KnockoutObservable<Lexicon>
     availableLexicons: KnockoutObservableArray<Lexicon>
     words: KnockoutObservable<string>
-    result: KnockoutObservableArray<Entry.Entry>
     validForm: KnockoutComputed<boolean>
+    entries: KnockoutObservableArray<Entry.Entry>
 
     constructor(lexicon: string, words: string) {
         this.availableLexicons = ko.observableArray([]);
         this.loadLexicons(lexicon);
         this.selectedLexicon = ko.observable(null);
         this.words = ko.observable(words);
-        this.result = ko.observableArray([]);
+        this.entries = ko.observableArray([]);
         this.validForm = ko.computed({
             read: () => {
                 return (this.selectedLexicon() != null && this.words().length > 0);
@@ -48,7 +48,30 @@ class SearchModel {
     };
 
     ipa(transcription: string): string {
-        return "ii.pp.aa";
+        let base_url = window.location.origin;
+        let itself = this;
+        let ipa = ""
+        if (itself.words().length === 0) {
+            alert("Cannot search for empty string!");
+            return;
+        };
+        if (itself.selectedLexicon() == null) {
+            alert("Lexicon(s) not specified, cannot search!");
+            return;
+        };
+        let r = new XMLHttpRequest();
+        let url = base_url + "/mapper/map?from=" + encodeURIComponent(itself.selectedLexicon().symbolSetName) + "&to=ipa&trans=" + encodeURIComponent(transcription);
+        r.open("GET", url, false);
+        r.onload = function () {
+            if (r.status === 200) {
+                ipa = JSON.parse(r.responseText).Result;
+            }
+            else {
+                alert("ERROR\n" + r.status + "\n" + r.responseText);
+            };
+        };
+        r.send();
+        return ipa;
     }
 
 
@@ -64,13 +87,13 @@ class SearchModel {
             return;
         };
         let r = new XMLHttpRequest();
-        let url = base_url + "/lexicon/lookup?lexicons=" + itself.selectedLexicon().name + "&words=" + itself.words(); // TODO: urlencode
+        let url = base_url + "/lexicon/lookup?lexicons=" + itself.selectedLexicon().name + "&words=" + encodeURIComponent(itself.words()); // TODO: urlencode
         r.open("GET", url);
         r.onload = function () {
             if (r.status === 200) {
                 let json: JSON = JSON.parse(r.responseText);
                 //console.log(JSON.stringify(json));
-                itself.result(Entry.json2entries(json));
+                itself.entries(Entry.json2entries(json));
             }
             else {
                 alert("ERROR\n" + r.status + "\n" + r.responseText);
