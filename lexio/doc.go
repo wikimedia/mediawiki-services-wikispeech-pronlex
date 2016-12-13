d464995509594de9a62f6139aac8a3011340c64b
Hanna Lindgren <han.lind@gmail.com>
2016-12-13 15:01:03 +0100

    doc

--- lexio/doc.go
+++ lexio/doc.go
@@ -1,2 +1,2 @@
-// Package lexio contains main routines for importing and exporting lexicon files to/from the database, and for converting some external file formats into a format readable by the pronlex package.
+// Package lexio contains main command line toos for importing and exporting lexicon files to/from the database, and for converting some external file formats into a format readable by the pronlex package.
 package lexio
