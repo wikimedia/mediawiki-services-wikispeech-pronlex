8f23c05bb47c9d0cb862cb6a6f82ec869fc06956
Hanna Lindgren <han.lind@gmail.com>
2016-12-13 14:01:20 +0100

    symbolset docs

--- symbolset/doc.go
+++ symbolset/doc.go
@@ -6,11 +6,12 @@ The basis of this package is a symbol set file on this tab-separated format:
 	DESCRIPTION	SYMBOL	IPA	IPA UNICODE	CATEGORY
 
 Sample lines (Swedish Wikispeech SAMPA):
+	DESCRIPTION	SYMBOL	IPA	IPA UNICODE	CATEGORY
 	sil	i:	iː	U+0069U+02D0	Syllabic
 	bok	b	b	U+0062	NonSyllabic
 	forna	rn	ɳ	U+0273	NonSyllabic
 
-For real world examples (used for unit tests), see the test_data folder in this package.
+Note that the header is required on the first line. For real world examples (used for unit tests), see the test_data folder in this package.
 
 **/
 package symbolset
