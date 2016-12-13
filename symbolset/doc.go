31ec7ef2f6ace7867dd0786d1cff0bb6e5766e8b
Hanna Lindgren <han.lind@gmail.com>
2016-12-13 14:00:29 +0100

    symbolset docs

--- symbolset/doc.go
+++ symbolset/doc.go
@@ -1,2 +1,14 @@
-// Package symbolset is used to define symbol sets and mapping between different sets, such as IPA to SAMPA, and so on.
+/* Package symbolset is used to define symbol sets and mapping between different sets, such as IPA to SAMPA, and so on.
+
+The basis of this package is a symbol set file on this tab-separated format:
+	DESCRIPTION	SYMBOL	IPA	IPA UNICODE	CATEGORY
+
+Sample lines (Swedish Wikispeech SAMPA):
+	sil	i:	iː	U+0069U+02D0	Syllabic
+	bok	b	b	U+0062	NonSyllabic
+	forna	rn	ɳ	U+0273	NonSyllabic
+
+For real world examples (used for unit tests), see the test_data folder in this package.
+
+**/
 package symbolset
