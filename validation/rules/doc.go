13834df9a2e24f1e05ad6303e9af300bb3687867
Hanna Lindgren <han.lind@gmail.com>
2016-12-13 13:22:05 +0100

    validation/rules doc.go

--- validation/rules/doc.go
+++ validation/rules/doc.go
@@ -1,2 +1,2 @@
-// Package rules contains a few general validation rule types. The validation design and interfaces can be found in package validation.
+// Package rules contains a few general validation rule types (rules.go), and project specific validators (validators.go). The validation design and interfaces can be found in package validation.
 package rules
