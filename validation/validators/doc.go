a7fa85395f8e35f467e5117334c923ea173cfc80
Hanna Lindgren <han.lind@gmail.com>
2016-12-14 15:56:31 +0100

    doc

--- validation/validators/doc.go
+++ validation/validators/doc.go
@@ -1,2 +1,2 @@
-// Package validators contains language and project specific validators. The validation design and interfaces can be found in package validation.
+// Package validators contains a validator service for caching loaded validators, and it contains language and project specific validators. The validation design and interfaces can be found in package validation.
 package validators
