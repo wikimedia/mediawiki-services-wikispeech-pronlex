1efc0a4d098d21e46076733906f8976af5749839
nikolaj <nikolaj.lindberg@stts.se>
2016-12-20 17:28:36 +0100

    client now gets users by http request

--- lexserver/user_db/test_server/src/user_db.ts
+++ lexserver/user_db/test_server/src/user_db.ts
@@ -1,14 +1,26 @@
-class User {
+const baseURL: string = window.location.origin;	
+
+interface User {
+
     name: string;
     roles: string;
     dbs: string;
 
-    constructor(name: string, roles: string, dbs: string) {
-        this.name = name;
-        this.roles = roles;
-        this.dbs = dbs;
-    }
 }
+
+
+// class User {
+
+//     name: string;
+//     roles: string;
+//     dbs: string;
+
+//     constructor(name: string, roles: string, dbs: string) {
+//         this.name = name;
+//         this.roles = roles;
+//         this.dbs = dbs;
+//     }
+// }
 
 class UserDB {
     //nisse = "NIZZE"
@@ -16,8 +28,26 @@ class UserDB {
     users: KnockoutObservableArray<User> = ko.observableArray<User>([]);
 
     getUsers(): void {
-        this.users.push(new User("nils", "thingy", "lexdb"))
-        this.users.push(new User("nuls", "thungy", "luxdb"))
+        //this.users.push( {name: "nils", roles: "thingy", dbs: "lexdb"})
+        //this.users.push( {name: "nuls", roles: "thungy", dbs: "loxdb"} )
+
+	let itself = this;
+	let url = baseURL + "/admin/user_db/list_users"
+	let r = new XMLHttpRequest();
+	console.log(url);
+	r.open("GET", url);
+	r.onload = function () {
+	    if (r.status == 200) {
+		// TODO How do you handle errors?
+		let u: User[] = <User[]>JSON.parse(r.responseText); 
+		itself.users(u);
+		
+	    } else {
+                console.log("readyState: " + r.readyState);
+                console.log("statusText: " + r.statusText);
+            }
+	}
+	r.send();
     }
 
 }
