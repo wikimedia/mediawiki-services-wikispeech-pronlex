a540dab2b5e73668b3653bc531813186f09609e4
Nikolaj Lindberg <nikolaj@stts.se>
2017-02-08 14:59:09 +0100

    broken test stuff

--- lexserver/user_db/test_server/src/user_db.ts
+++ lexserver/user_db/test_server/src/user_db.ts
@@ -1,4 +1,4 @@
-const baseURL: string = window.location.origin;	
+const baseURL: string = window.location.origin;
 
 interface User {
 
@@ -25,40 +25,71 @@ interface User {
 class UserDB {
     //nisse = "NIZZE"
 
-    //deleteUser;
+    //zelf; // = this; // = null; // = this;
+    //constructor() {
+    // 	this.zelf = this;
+	//zelf.getUsers = getUsers();
+    //};
+ 
     
+    //deleteUser;
+
+    public itself: any = this;
+
+    message: string = "";
+
     users: KnockoutObservableArray<User> = ko.observableArray<User>([]);
 
-    getUsers(): void {
+     constructor() {
+     	this.itself = this;
+     }
+    
+    public  getUsers = () => {
         //this.users.push( {name: "nils", roles: "thingy", dbs: "lexdb"})
         //this.users.push( {name: "nuls", roles: "thungy", dbs: "loxdb"} )
-
-	console.log("GETTING USER LIST");
 	
-	let itself = this;
-	let url = baseURL + "/admin/user_db/list_users"
-	let r = new XMLHttpRequest();
-	console.log(url);
-	r.open("GET", url);
-	r.onload = function () {
-	    if (r.status == 200) {
-		// TODO How do you handle errors?
-		let u: User[] = <User[]>JSON.parse(r.responseText); 
-		itself.users(u);
-		
-	    } else {
+        console.log("GETTING USER LIST");
+	
+        let itself = this;
+        let url = baseURL + "/admin/user_db/list_users"
+        let r = new XMLHttpRequest();
+        console.log(url);
+        r.open("GET", url);
+        r.onload = function () {
+            if (r.status == 200) {
+                // TODO How do you handle errors?
+                let u: User[] = <User[]>JSON.parse(r.responseText);
+                itself.users(u);
+
+            } else {
                 console.log("readyState: " + r.readyState);
                 console.log("statusText: " + r.statusText);
             }
+        }
+        r.send();
+    }
+    
+    deleteUser(user: User): void {
+	//let f = itself.getUsers;
+        console.log(user.name);
+        let itself = this;
+        let url = baseURL + "/admin/user_db/delete_user?name="+ user.name;
+        let r = new XMLHttpRequest();
+	r.open("GET", url);
+
+
+	r.onload = () => {
+	    if(r.status == 200) {
+		//let x = new UserDB();
+		//x.getUsers();
+		this.getUsers()
+	    }
 	}
 	r.send();
     }
-
-    deleteUser = (user: User) => {
-	console.log(user);
-    }
-    addUser() {
-	console.log("YEAH, new user");
+    
+    addUser(): void {
+        console.log("YEAH, new user");
     }
 }
 
