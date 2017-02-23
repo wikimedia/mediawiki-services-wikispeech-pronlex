69653292b251ed8145690d0a0524678b0f698f70
Hanna Lindgren <han.lind@gmail.com>
2017-02-23 14:02:59 +0100

    r

--- lexserver/mapper.go
+++ lexserver/mapper.go
@@ -46,6 +46,9 @@ func mapMapperHandler(w http.ResponseWriter, r *http.Request) {
 	fromName := r.FormValue("from")
 	toName := r.FormValue("to")
 	trans := trimTrans(r.FormValue("trans"))
+	log.Println("from", fromName)
+	log.Println("to", toName)
+	log.Println("trans", trans)
 	if len(strings.TrimSpace(fromName)) == 0 {
 		msg := fmt.Sprintf("input symbol set should be specified by variable 'from'")
 		log.Println(msg)
