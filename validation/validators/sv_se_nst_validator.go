5a4b3a0dfb754ae2a2bcf6798e1a10567abb6463
Hanna Lindgren <han.lind@gmail.com>
2017-02-24 17:38:33 +0100

    validation rules test suite, work in process

--- validation/validators/sv_se_nst_validator.go
+++ validation/validators/sv_se_nst_validator.go
@@ -65,6 +65,18 @@ func newSvSeNstValidator(symbolset symbolset.SymbolSet) (validation.Validator, e
 				Level:   "Fatal",
 				Message: "Stress can only be used in syllable initial position",
 				Re:      stressFirst,
+				// Accept: []lex.Entry{
+				// 	lex.Entry{Transcriptions: []lex.Transcription{
+				// 		lex.Transcription{Strn: "\" A: . p a"}}},
+				// 	lex.Entry{Transcriptions: []lex.Transcription{
+				// 		lex.Transcription{Strn: "p O . \" E N"}}},
+				// },
+				// Reject: []lex.Entry{
+				// 	lex.Entry{Transcriptions: []lex.Transcription{
+				// 		lex.Transcription{Strn: "A: \" . p a"}}},
+				// 	lex.Entry{Transcriptions: []lex.Transcription{
+				// 		lex.Transcription{Strn: "s k r \" A: . p a"}}},
+				// },
 			},
 			rules.RequiredTransRe{
 				Name:    "syllabic",
