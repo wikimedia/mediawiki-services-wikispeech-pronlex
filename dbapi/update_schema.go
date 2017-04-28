53ce99d426e99a6c9366c912f034fb5a1dc96a20
Nikolaj Lindberg <nikolaj@stts.se>
2017-04-28 15:02:11 +0200

    update schema

--- dbapi/update_schema.go
+++ dbapi/update_schema.go
@@ -17,12 +17,30 @@ func UpdateSchema(dbFile string) error {
 
 	db, err := sql.Open("sqlite3", dbFile)
 	if err != nil {
-		return fmt.Errorf("UpdateSchama: %v", err)
+		return fmt.Errorf("UpdateSchema: %v", err)
 	}
 	defer db.Close()
 
 	var userVersion int
-	err = db.QueryRow("PRAGMA user_verion").Scan(&userVersion)
+	err = db.QueryRow("PRAGMA user_version").Scan(&userVersion)
+	if err != nil {
+
+		return fmt.Errorf("UpdateSchema: %v", err)
+	}
+	fmt.Fprintf(os.Stderr, "UpdateSchema: current user_version: %d\n", userVersion)
+
+	if userVersion < 1 {
+		// Substitute faulty version of trigger
+
+		rez, err := db.Exec("DROP TRIGGER insertPref")
+		if err != nil {
+			return fmt.Errorf("UpdateSchema failed when dropping trigger : %v", err)
+		}
+		n, err := rez.RowsAffected()
+		if err != nil {
+			return fmt.Errorf("UpdateSchema failed when calling RowsAffected : %v", err)
+		}
+	}
 
 	return nil
 }
