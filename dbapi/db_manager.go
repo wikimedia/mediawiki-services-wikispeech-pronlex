e3522bd8af49b176b7b850fcd2be0dec0c4dbafd
Nikolaj Lindberg <nikolaj@stts.se>
2017-06-19 15:11:53 +0200

    ListLexicons

--- dbapi/db_manager.go
+++ dbapi/db_manager.go
@@ -20,14 +20,18 @@ func (dmb DBManager) AddDB(name string, db *sql.DB) error {
 	if "" == strings.TrimSpace(name) {
 		return fmt.Errorf("DBManager.AddDB: illegal argument: name must not be empty")
 	}
+	if strings.Contains(name, ":") {
+		return fmt.Errorf("DBManager.AddDB: illegal argument: name must not contain ':'")
+	}
 	if nil == db {
 		return fmt.Errorf("DBManager.AddDB: illegal argument: db must not be nil")
 	}
 
 	dmb.Lock()
 	defer dmb.Unlock()
+
 	if _, ok := dmb.dbs[name]; ok {
-		return fmt.Errorf("DBManager.AddDB: db already exists (use RemoveDB to remove it): '%s'", name)
+		return fmt.Errorf("DBManager.AddDB: db already exists: '%s'", name)
 	}
 
 	dmb.dbs[name] = db
@@ -46,4 +50,37 @@ func (dmb DBManager) RemoveDB(name string) error {
 	delete(dmb.dbs, name)
 
 	return nil
+}
+
+func (dbm DBManager) ListDBNames() []string {
+	var res []string
+
+	dbm.RLock()
+	defer dbm.RUnlock()
+
+	for k, _ := range dbm.dbs {
+		res = append(res, k)
+	}
+
+	return res
+}
+
+func (dbm DBManager) ListLexicons() ([]string, error) {
+	var res []string
+
+	dbm.RLock()
+	defer dbm.RUnlock()
+
+	for name, db := range dbm.dbs {
+		lexs, err := ListLexicons(db)
+		if err != nil {
+			return res, fmt.Errorf("DBManager.ListLexicons : %v", err)
+		}
+
+		for _, l := range lexs {
+			res = append(res, name+":"+l.Name)
+		}
+	}
+
+	return res, nil
 }
