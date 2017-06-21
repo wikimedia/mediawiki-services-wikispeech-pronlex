7753b5535527773a9411bf799f14584143d30c65
Hanna Lindgren <han.lind@gmail.com>
2017-06-21 15:49:18 +0200

    r

--- symbolset/converter/doc.go
+++ symbolset/converter/doc.go
@@ -7,35 +7,35 @@ Each converter is defined in a .cnv file including symbol set names and conversi
 Tests can also be added to verify how the conversion works.
 
 
-Sample file for US English Sampa to Swedish Sampa:
+Sample file for US English Sampa to Swedish Sampa enusampa_svsampa.cnv:
 
-FROM	en-us_ws-sampa
-TO	sv-se_ws-sampa
+     FROM	en-us_ws-sampa
+     TO	sv-se_ws-sampa
 
-RE	^T	t
+     RE	^T	t
 
-SYMBOL	dZ	d j
-SYMBOL	tS	t rs
-SYMBOL	i	I
-SYMBOL	D	d
-SYMBOL	T	t
-SYMBOL	S	rs
-SYMBOL	z	s
-SYMBOL	Z	s
-SYMBOL	w	v
-SYMBOL	A	a
-SYMBOL	u	U
-SYMBOL	V	a
-SYMBOL	r=	@ r
-SYMBOL	aU	au
-SYMBOL	OI	O j
-SYMBOL	@U	u:
-SYMBOL	EI	e j
-SYMBOL	AI	a j
-SYMBOL	'	"
+     SYMBOL	dZ	d j
+     SYMBOL	tS	t rs
+     SYMBOL	i	I
+     SYMBOL	D	d
+     SYMBOL	T	t
+     SYMBOL	S	rs
+     SYMBOL	z	s
+     SYMBOL	Z	s
+     SYMBOL	w	v
+     SYMBOL	A	a
+     SYMBOL	u	U
+     SYMBOL	V	a
+     SYMBOL	r=	@ r
+     SYMBOL	aU	au
+     SYMBOL	OI	O j
+     SYMBOL	@U	u:
+     SYMBOL	EI	e j
+     SYMBOL	AI	a j
+     SYMBOL	'	"
 
-TEST	T i s	t I s
-TEST	D i s	d I s
+     TEST	T i s	t I s
+     TEST	D i s	d I s
 
 
 For real world examples (used for unit tests), see the test_data folder: https://github.com/stts-se/pronlex/tree/master/symbolset/converter/test_data
