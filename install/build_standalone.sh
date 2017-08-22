8b43dd9e0c63db03349a46e4d5a87f30d4978e16
Hanna Lindgren <han.lind@gmail.com>
2017-08-22 15:50:38 +0200

    build script

--- install/build_standalone.sh
+++ install/build_standalone.sh
@@ -20,29 +20,33 @@ ARLEX=ar_ar_tst_lex.db
 
 mkdir -p $APP_DIR
 
-git clone https://github.com/stts-se/lexdata.git $APP_DIR/lexdata
+if [ -d "$APP_DIR/lexdata" ]; then
+    cd $APP_DIR/lexdata && git pull && cd -
+    else
+	git clone https://github.com/stts-se/lexdata.git $APP_DIR/lexdata
+fi
 
-mkdir $APP_DIR/db_files
-mkdir $APP_DIR/symbol_sets
+mkdir -p $APP_DIR/db_files
+mkdir -p $APP_DIR/symbol_sets
 
 cp $APP_DIR/lexdata/*/*/*.sym $APP_DIR/symbol_sets/
 cp $APP_DIR/lexdata/mappers.txt $APP_DIR/symbol_sets/
 cp $APP_DIR/lexdata/converters/*.cnv $APP_DIR/symbol_sets/
 
 
-echo $SVLEX
-createEmptyDB $APP_DIR/db_files/$SVLEX
-importLex -replace $APP_DIR/db_files/$SVLEX sv-se.nst $APP_DIR/lexdata/sv-se/nst/swe030224NST.pron-ws.utf8.gz sv-se_ws-sampa $APP_DIR/symbol_sets
+# echo "IMPORT: $SVLEX"
+# createEmptyDB $APP_DIR/db_files/$SVLEX
+# importLex -replace $APP_DIR/db_files/$SVLEX sv-se.nst $APP_DIR/lexdata/sv-se/nst/swe030224NST.pron-ws.utf8.gz sv-se_ws-sampa $APP_DIR/symbol_sets
 
-echo $NOBLEX
-createEmptyDB $APP_DIR/db_files/$NOBLEX
-importLex -replace $APP_DIR/db_files/$NOBLEX nb-no.nst $APP_DIR/lexdata/nb-no/nst/nor030224NST.pron-ws.utf8.gz nb-no_ws-sampa $APP_DIR/symbol_sets
+# echo "IMPORT: $NOBLEX"
+# createEmptyDB $APP_DIR/db_files/$NOBLEX
+# importLex -replace $APP_DIR/db_files/$NOBLEX nb-no.nst $APP_DIR/lexdata/nb-no/nst/nor030224NST.pron-ws.utf8.gz nb-no_ws-sampa $APP_DIR/symbol_sets
 
-echo $AMLEX
+echo "IMPORT: $AMELEX"
 createEmptyDB $APP_DIR/db_files/$AMELEX
 importLex -replace $APP_DIR/db_files/$AMELEX en-us.cmu $APP_DIR/lexdata/en-us/cmudict/cmudict-0.7b-ws.utf8 en-us_ws-sampa $APP_DIR/symbol_sets
 
-echo $ARLEX
+echo "IMPORT: $ARLEX"
 createEmptyDB $APP_DIR/db_files/$ARLEX
 importLex -replace $APP_DIR/db_files/$ARLEX ar-test $APP_DIR/lexdata/ar/TEST/ar_TEST.pron-ws.utf8 ar_ws-sampa $APP_DIR/symbol_sets
 
