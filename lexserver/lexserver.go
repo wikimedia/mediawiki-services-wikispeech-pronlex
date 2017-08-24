92abb828e893ade6ef3c3416669c1b5edf4ac424
Hanna Lindgren <han.lind@gmail.com>
2017-08-24 13:18:12 +0200

    go vet

--- lexserver/lexserver.go
+++ lexserver/lexserver.go
@@ -617,7 +617,8 @@ Default ports:
 		fmt.Fprintf(os.Stderr, "\n")
 		log.Println("lexserver: shutting down...")
 
-		ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
 		defer s.Shutdown(ctx)
 
 		// shut down databases nicely
