356e8c70570a05dd4d6e4b589fb3002ffdbfda18
Hanna Lindgren <han.lind@gmail.com>
2017-09-22 11:18:10 +0200

    documentation of lexicon file format

--- line/doc.go
+++ line/doc.go
@@ -1,7 +1,9 @@
-/* Package line is used to define lexicon line formats for parsing input and printing output.
+/*Package line is used to define lexicon line formats for parsing input and printing output.
 
 Interfaces:
+
 * Format - simple line format definition (field names and indices)
+
 * Parser - a more complex parser, containing a Format definition, but also adds the possibility to write specific code for parsing that cannot be handeled by the Format specs alone (multi-value fields, etc).
 
 
