7a588e633173ba0a3240d4af30628524e1de3c72
Hanna Lindgren <han.lind@gmail.com>
2017-11-28 10:23:22 +0100

    r

--- install/setup.sh
+++ install/setup.sh
@@ -17,6 +17,9 @@ fi
 APPDIR=$1
 LEXDB=wikispeech_testdb.db
 
+DEMOFILES=$GOPATH/src/github.com/stts-se/pronlex/lexserver/demo_files
+
+
 if [ -z "$GOPATH" ] ; then
     echo "[$CMD] The GOPATH environment variable is required!" >&2
     exit 1
@@ -46,10 +49,9 @@ function initial_setup {
     mkdir -p $APPDIR/symbol_sets || exit 1
     mkdir -p $APPDIR/db_files || exit 1
 
-    cp $GOPATH/src/github.com/stts-se/pronlex/lexserver/demo_files/*.sym $APPDIR/symbol_sets/ || exit 1
-    cp $GOPATH/src/github.com/stts-se/pronlex/lexserver/demo_files/*.cnv $APPDIR/symbol_sets/ || exit 1
-    echo "" >> $APPDIR/symbol_sets/mappers.txt || exit 1
-    cat $GOPATH/src/github.com/stts-se/pronlex/lexserver/demo_files/mappers.txt >> $APPDIR/symbol_sets/mappers.txt || exit 1
+    cp $DEMOFILES/*.sym $APPDIR/symbol_sets/ || exit 1
+    cp $DEMOFILES/*.cnv $APPDIR/symbol_sets/ || exit 1
+    cp $DEMOFILES/mappers.txt $APPDIR/symbol_sets/ || exit 1
 
     cp $GOPATH/src/github.com/stts-se/pronlex/install/import.sh $APPDIR || exit 1
     cp $GOPATH/src/github.com/stts-se/pronlex/install/start_server.sh $APPDIR || exit 1
@@ -72,28 +74,6 @@ else
 fi
 
 
-nConverters=`ls $APPDIR/lexdata/converters/*.cnv 2> /dev/null |wc -l`
-nSymSets=`ls $APPDIR/lexdata/*/*/*.sym 2> /dev/null |wc -l`
-if [ -d "$APPDIR/lexdata" ] && [ $nSymSets != 0 ] || [ $nConverters != 0 ]; then
-    cd $APPDIR/lexdata && git pull && cd -
-    KEEP=1
-else
-    if git clone https://github.com/stts-se/lexdata.git $APPDIR/lexdata.git; then
-	echo -n "" # OK
-    else
-	echo "[$CMD] git clone failed" >&2
-	exit 1
-    fi
-fi
-
-mkdir -p $APPDIR/db_files || exit 1
-mkdir -p $APPDIR/symbol_sets || exit 1
-
-cp $APPDIR/lexdata.git/*/*/*.sym $APPDIR/symbol_sets/ || exit 1
-echo "" >> $APPDIR/symbol_sets/mappers.txt || exit 1
-cat $APPDIR/lexdata.git/mappers.txt >> $APPDIR/symbol_sets/mappers.txt || exit 1
-cp $APPDIR/lexdata.git/converters/*.cnv $APPDIR/symbol_sets/ || exit 1
-
 
 ### LEXDATA IMPORT
 
@@ -102,7 +82,7 @@ echo "IMPORT: $LEXDB:sv" >&2
 
 CMDDIR="$GOPATH/src/github.com/stts-se/pronlex/cmd/lexio"
 
-if zcat $APPDIR/lexdata.git/sv-se/nst/swe030224NST.pron-ws.utf8.gz | egrep -wi "apa|hund|färöarna|det|här|är|ett|test" > $APPDIR/lexdata.git/sv-se/nst/swe030224NST.pron-ws.utf8.for_testing && go run $CMDDIR/importLex/importLex.go $APPDIR/db_files/$LEXDB sv sv_SE $APPDIR/lexdata.git/sv-se/nst/swe030224NST.pron-ws.utf8.for_testing sv-se_ws-sampa $APPDIR/symbol_sets ; then
+if go run $CMDDIR/importLex/importLex.go $APPDIR/db_files/$LEXDB sv sv_SE $DEMOFILES/swe030224NST.pron-ws.utf8.for_testing sv-se_ws-sampa $APPDIR/symbol_sets ; then
     echo -n ""
 else
     echo "$LEXDB:sv FAILED" >&2
@@ -111,7 +91,7 @@ fi
 
 echo "" >&2
 echo "IMPORT: $LEXDB:nb" >&2
-if zcat $APPDIR/lexdata.git/nb-no/nst/nor030224NST.pron-ws.utf8.gz | egrep -wi "apa|hund|det|ær|test|banebrytende" > $APPDIR/lexdata.git/nb-no/nst/nor030224NST.pron-ws.utf8.for_testing &&  go run $CMDDIR/importLex/importLex.go $APPDIR/db_files/$LEXDB nb nb_NO $APPDIR/lexdata.git/nb-no/nst/nor030224NST.pron-ws.utf8.for_testing nb-no_ws-sampa $APPDIR/symbol_sets ; then
+if go run $CMDDIR/importLex/importLex.go $APPDIR/db_files/$LEXDB nb nb_NO $DEMOFILES/nor030224NST.pron-ws.utf8.for_testing nb-no_ws-sampa $APPDIR/symbol_sets ; then
     echo -n ""
 else
     echo "$LEXDB:nb FAILED" >&2
@@ -120,7 +100,7 @@ fi
 
 echo "" >&2
 echo "IMPORT: $LEXDB:enu" >&2
-if cat $APPDIR/lexdata.git/en-us/cmudict/cmudict-0.7b-ws.utf8 | egrep -w "test|a|children" > $APPDIR/lexdata.git/en-us/cmudict/cmudict-0.7b-ws.utf8.for_testing &&  go run $CMDDIR/importLex/importLex.go $APPDIR/db_files/$LEXDB enu en_US $APPDIR/lexdata.git/en-us/cmudict/cmudict-0.7b-ws.utf8.for_testing en-us_ws-sampa $APPDIR/symbol_sets ; then
+if go run $CMDDIR/importLex/importLex.go $APPDIR/db_files/$LEXDB enu en_US $DEMOFILES/cmudict-0.7b-ws.utf8.for_testing en-us_ws-sampa $APPDIR/symbol_sets ; then
     echo -n ""
 else
     echo "$LEXDB:enu FAILED" >&2
@@ -129,7 +109,7 @@ else
 
 echo "" >&2
 echo "IMPORT: $LEXDB:ar" >&2
-if  go run $CMDDIR/importLex/importLex.go $APPDIR/db_files/$LEXDB ar ar_AR $APPDIR/lexdata.git/ar/TEST/ar_TEST.pron-ws.utf8 ar_ws-sampa $APPDIR/symbol_sets ; then
+if  go run $CMDDIR/importLex/importLex.go $APPDIR/db_files/$LEXDB ar ar_AR $DEMOFILES/ar_TEST.pron-ws.utf8.for_testing ar_ws-sampa $APPDIR/symbol_sets ; then
     echo -n ""
 else
     echo "$LEXDB:ar FAILED" >&2
