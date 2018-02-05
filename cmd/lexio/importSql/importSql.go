98f9be514614ab9affe7d901d1119706afa354b5
Hanna Lindgren <han.lind@gmail.com>
2018-02-05 11:02:42 +0100

    importSql

--- cmd/lexio/importSql/importSql.go
+++ cmd/lexio/importSql/importSql.go
@@ -1,39 +1,42 @@
 package main
 
 import (
-	// "database/sql"
-	"fmt"
-	//_ "github.com/mattn/go-sqlite3"
 	"bytes"
+	"compress/gzip"
+	"fmt"
+	"io"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 // SQL DUMP:
-// sqlite3 en_am_cmu_lex.db
-// sqlite> .output "en_am_cmu_lex.sql"
-// sqlite> .dump
-// sqlite> .exit
+// sqlite3 <dbFile> .dump | gzip -c > <dumpFile>
 
-// TODO: check PRAGMA user_version to ensure matches the current schema.go
-// TODO: dump new db and compare to original dump file
+// SQL LOAD:
+// gunzip -c <dumpFile> | sqlite3 <dbFile>
+
+// TODO:
+// * Jämför dumpens PRAGMA user_version med aktuellt schema (dbapi/schema.go). Vägra importera om det inte matchar.
+// * Test: dump new db and compare to original dump file
+// * Test: köra några enkla sanity check-anrop, t.ex. select count entries osv..
 
 func main() {
 
 	if len(os.Args) != 3 {
-		fmt.Fprintf(os.Stderr, "USAGE:\nimportSql <SQL DUMP FILE> <NEW DB FILE>\n")
+		fmt.Fprintf(os.Stderr, "USAGE:\nimportSql <SQL DUMP FILE (.sql or .sql.gz)> <NEW DB FILE>\n")
 		os.Exit(1)
 	}
 
 	var sqlDumpFile = os.Args[1]
 	var dbFile = os.Args[2]
 
-	fmt.Println(sqlDumpFile)
-	fmt.Println(dbFile)
+	fmt.Printf("Input file: %s\n", sqlDumpFile)
+	fmt.Printf("Output db: %s\n", dbFile)
 
 	_, err := os.Stat(sqlDumpFile)
 	if err != nil {
-		fmt.Printf("Cannot find db file: %v\n", err)
+		fmt.Printf("Input file does not exist : %v\n", err)
 		return
 	}
 
@@ -42,20 +45,56 @@ func main() {
 		return
 	}
 
-	// cat <sqlDumpFile> | sqlite3 <dbFile>
-	cmd := exec.Command("sqlite3", dbFile)
-	stream, err := os.Open(sqlDumpFile)
-	if err != nil {
-		fmt.Printf("Couldn't open sql dump file %s for reading : %v\n", sqlDumpFile, err)
+	var fileStream io.Reader
+
+	if strings.HasSuffix(sqlDumpFile, ".sql") {
+		// cat <sqlDumpFile> | sqlite3 <dbFile>
+		fs, err := os.Open(sqlDumpFile)
+		if err != nil {
+			fmt.Printf("Couldn't open sql dump file %s for reading : %v\n", sqlDumpFile, err)
+			return
+		}
+		fileStream = io.Reader(fs)
+
+	} else if strings.HasSuffix(sqlDumpFile, ".sql.gz") {
+		// zcat <sqlDumpFile> | gunzip -c | sqlite3 <dbFile>
+
+		fh, err := os.Open(sqlDumpFile)
+		defer fh.Close()
+		if err != nil {
+			var msg = fmt.Sprintf("Couldn't open file : %v", err)
+			fmt.Println(msg)
+			return
+		}
+
+		if strings.HasSuffix(sqlDumpFile, ".gz") {
+			gz, err := gzip.NewReader(fh)
+			if err != nil {
+				var msg = fmt.Sprintf("Couldn't to open gz reader : %v", err)
+				fmt.Println(msg)
+				return
+			}
+			fileStream = io.Reader(gz)
+		}
+
+	} else {
+		fmt.Println("Unknown file type: %s. Expected .sql or .sql.gz", sqlDumpFile)
+		return
 	}
-	cmd.Stdin = stream
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err = cmd.Run()
+
+	sqliteCmd := exec.Command("sqlite3", dbFile)
+	sqliteCmd.Stdin = fileStream
+	var sqliteOut bytes.Buffer
+	sqliteCmd.Stdout = &sqliteOut
+	sqliteCmd.Stderr = os.Stderr
+	err = sqliteCmd.Run()
 	if err != nil {
 		fmt.Printf("Couldn't load sql dump %s into db %s : %v\n", sqlDumpFile, dbFile, err)
+		return
 	}
-	//fmt.Println(out.String())
+	if len(sqliteOut.String()) > 0 {
+		fmt.Println(sqliteOut.String())
+	}
 
 	fmt.Printf("Imported %s into db %s\n", sqlDumpFile, dbFile)
 
