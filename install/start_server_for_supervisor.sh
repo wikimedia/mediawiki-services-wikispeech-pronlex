84d18b1aa0bd245503697bb56f5a1f5360e267f7
Hanna <hanna@stts.se>
2018-04-27 15:25:08 +0200

    install/start_server_for_supervisor.sh

---
+++ install/start_server_for_supervisor.sh
@@ -0,0 +1,68 @@
+#!/bin/bash 
+
+#############################################################
+### SERVER STARTUP SCRIPT
+## 1. STARTS A TEST SERVER AND RUNS A SET OF TESTS
+## 2. SHUTS DOWN TEST SERVER
+## 3. IF NO ERRORS ARE FOUND, THE STANDARD SERVER IS STARTED USING THE DEFAULT PORT
+
+
+CMD=`basename $0`
+PORT="8787"
+export GOPATH=/home/hanna/go
+export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
+
+while getopts ":hp:a:" opt; do
+    case $opt in
+	h)
+	    echo "
+[$CMD] SERVER STARTUP SCRIPT
+   1. STARTS A TEST SERVER AND RUNS A SET OF TESTS
+   2. SHUTS DOWN TEST SERVER
+   3. IF NO ERRORS ARE FOUND, THE STANDARD SERVER IS STARTED USING THE DEFAULT PORT
+
+Options:
+  -h help
+  -a appdir (required)
+  -p port   (default: $PORT)
+
+EXAMPLE INVOCATION: $CMD -a lexserver_files
+" >&2
+	    exit 1
+	    ;;
+	a)
+	    APPDIR=$OPTARG
+	    ;;
+	p)
+	    PORT=$OPTARG
+	    ;;
+	\?)
+	    echo "Invalid option: -$OPTARG" >&2
+	    exit 1
+	    ;;
+    esac
+done
+
+if [ -z "$APPDIR" ] ; then
+    echo "[$CMD] APPDIR must be specified using -a!" >&2
+    exit 1
+fi
+
+if [ -z "$GOPATH" ] ; then
+    echo "[$CMD] The GOPATH environment variable is required!" >&2
+    exit 1
+fi
+
+shift $(expr $OPTIND - 1 )
+
+if [ $# -ne 0 ]; then
+    echo "[$CMD] invalid option(s): $*" >&2
+    exit 1
+fi
+
+APPDIRABS=`readlink -f $APPDIR`
+
+
+CMDDIR="$GOPATH/src/github.com/stts-se/pronlex/lexserver"
+switches="-ss_files $APPDIRABS/symbol_sets/ -db_files $APPDIRABS/db_files/ -static $CMDDIR/static"
+cd $GOPATH/src/github.com/stts-se/pronlex/lexserver && go run *.go $switches -test && go run *.go $switches $PORT
