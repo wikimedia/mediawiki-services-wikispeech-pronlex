ef95b3bd1a33c8024f6ef7b93543621ef21c6d88
Hanna Lindgren <han.lind@gmail.com>
2018-05-29 11:30:08 +0200

    createEmptyLexicon with unit tests

--- cmd/lexio/createEmptyLexicon/createEmptyLexicon.go
+++ cmd/lexio/createEmptyLexicon/createEmptyLexicon.go
@@ -13,6 +13,51 @@ import (
 	"github.com/stts-se/pronlex/lex"
 )
 
+func createEmptyLexicon(dbPath string, lexRefX lex.LexRefWithInfo, locale string, createDbIfNotExists bool, closeAfter bool) error {
+	dbm := dbapi.NewDBManager()
+	lexRef := lexRefX.LexRef // db + lexname without ssName
+
+	if closeAfter {
+		defer dbm.CloseDB(lexRef.DBRef)
+	}
+
+	if _, err := os.Stat(dbPath); os.IsNotExist(err) {
+		if createDbIfNotExists {
+			err := dbm.DefineSqliteDB(lexRef.DBRef, dbPath)
+			if err != nil {
+				return fmt.Errorf("couldn't create db %s : %v", dbPath, err)
+			}
+			fmt.Fprintf(os.Stderr, "created db %s\n", dbPath)
+		} else {
+			return fmt.Errorf("db does not exist %s : %v", dbPath, err)
+		}
+	} else {
+		err := dbm.OpenDB(lexRef.DBRef, dbPath)
+		if err != nil {
+			return fmt.Errorf("couldn't open db %s : %v", dbPath, err)
+		}
+	}
+
+	if !dbm.ContainsDB(lexRef.DBRef) {
+		return fmt.Errorf("db should be registered in dbm, but wasn't : %s", lexRef.DBRef)
+	}
+
+	exists, err := dbm.LexiconExists(lexRef)
+	if err != nil {
+		return fmt.Errorf("couldn't lookup lexicon reference %s : %v", lexRef.String(), err)
+	}
+	if exists {
+		return fmt.Errorf("couldn't create lexicon that already exists: %s", lexRef.String())
+	}
+
+	err = dbm.DefineLexicon(lexRefX.LexRef, lexRefX.SymbolSetName, locale)
+	if err != nil {
+		return fmt.Errorf("couldn't create lexicon : %v", err)
+	}
+
+	return nil
+}
+
 func main() {
 
 	var createDbIfNotExists1 = flag.Bool("create_db", false, "create db if it doesn't exist")
@@ -23,7 +68,7 @@ func main() {
      $ createEmptyLexicon [flags] <DB PATH> <LEX NAME> <SYMBOL SET NAME> <LOCALE>
 
 Flags:
-     -create_db   bool    create db if it doesn't exist
+     -create_db   bool    create db if it doesn't exist (alias -c)
      -help        bool    print help and exit
 `
 
@@ -49,47 +94,17 @@ Flags:
 	_, dbFile := filepath.Split(dbPath)
 	dbExt := filepath.Ext(dbFile)
 	dbName := dbFile[0 : len(dbFile)-len(dbExt)]
-	lexRef := lex.NewLexRef(dbName, lexName)
+	lexRefX := lex.NewLexRefWithInfo(dbName, lexName, ssName)
+	closeAfter := true
 
 	dbapi.Sqlite3WithRegex()
-	dbm := dbapi.NewDBManager()
-
-	if _, err := os.Stat(dbPath); os.IsNotExist(err) {
-		if createDbIfNotExists {
-			err := dbm.DefineSqliteDB(lexRef.DBRef, dbPath)
-			if err != nil {
-				msg := fmt.Sprintf("[createEmptyLexicon] couldn't create db %s : %v", dbPath, err)
-				fmt.Fprintln(os.Stderr, msg)
-				os.Exit(1)
-			}
-			fmt.Fprintf(os.Stderr, "[createEmptyLexicon] created db %s\n", dbPath)
-		} else {
-			msg := fmt.Sprintf("[createEmptyLexicon] db does not exist %s : %v", dbPath, err)
-			fmt.Fprintln(os.Stderr, msg)
-			os.Exit(1)
-		}
-	}
-
-	exists, err := dbm.LexiconExists(lexRef)
+	err := createEmptyLexicon(dbPath, lexRefX, locale, createDbIfNotExists, closeAfter)
 	if err != nil {
-		msg := fmt.Sprintf("[createEmptyLexicon] couldn't lookup lexicon reference %s : %v", lexRef.String(), err)
-		fmt.Fprintln(os.Stderr, msg)
-		os.Exit(1)
-	}
-	if exists {
-		msg := fmt.Sprintf("[createEmptyLexicon] couldn't create lexicon that already exists: %s", lexRef.String())
-		fmt.Fprintln(os.Stderr, msg)
+		fmt.Fprintln(os.Stderr, fmt.Errorf("[%s] %v", "createEmptyLexicon", err))
 		os.Exit(1)
 	}
 
-	err = dbm.DefineLexicon(lexRef, ssName, locale)
-	if err != nil {
-		msg := fmt.Sprintf("[createEmptyLexicon] couldn't create lexicon : %v", err)
-		fmt.Fprintln(os.Stderr, msg)
-		os.Exit(1)
-	}
 	fmt.Fprintf(os.Stderr, "[createEmptyLexicon] created lexicon %s in %s\n", lexName, dbPath)
 	fmt.Fprintf(os.Stderr, "[createEmptyLexicon]  > symbolset: %s\n", ssName)
 	fmt.Fprintf(os.Stderr, "[createEmptyLexicon]  > locale:    %s\n", locale)
-
 }
---
+++ cmd/lexio/createEmptyLexicon/createEmptyLexicon_test.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"path"
+	"testing"
+
+	"github.com/stts-se/pronlex/dbapi"
+	"github.com/stts-se/pronlex/lex"
+)
+
+var fsExpTrans = "Expected: '%v' got: %v'"
+
+func TestProperCloseDontRemoveLockFiles(t *testing.T) {
+
+	// 1. SETUP
+	tmpDir, err := ioutil.TempDir(os.TempDir(), "pronlex-tmp-")
+	defer os.RemoveAll(tmpDir)
+	if err != nil {
+		t.Errorf("didn't expect error here, found : %v", err)
+		return
+	}
+
+	dbPath := path.Join(tmpDir, "createEmptyLexicon1.db")
+	lexName := "lex_test1"
+	ssName := "ss_test1"
+	locale := "en_US"
+	lexRefX := lex.NewLexRefWithInfo(dbPath, lexName, ssName)
+	closeAfter := false
+	createDB := true // if not exists
+
+	// 2. CREATE A NEW LEXICON IN A NEW DB
+	err = createEmptyLexicon(dbPath, lexRefX, locale, createDB, closeAfter)
+	if err != nil {
+		t.Errorf("didn't expect error here, found : %v", err)
+		return
+	}
+
+	// 3. DO NOT REMOVE shm/wal FILES
+
+	// 4. OPEN THE SAME DB
+	lexRefX = lex.NewLexRefWithInfo(dbPath, lexName+"-2", ssName)
+	err = createEmptyLexicon(dbPath, lexRefX, locale, createDB, closeAfter)
+	if err != nil {
+		t.Errorf("didn't expect error here, found : %v", err)
+		return
+	}
+}
+
+func TestProperCloseRemoveLockFiles(t *testing.T) {
+
+	// 1. SETUP
+	tmpDir, err := ioutil.TempDir(os.TempDir(), "pronlex-tmp-")
+	defer os.RemoveAll(tmpDir)
+	if err != nil {
+		t.Errorf("didn't expect error here, found : %v", err)
+		return
+	}
+
+	dbPath := path.Join(tmpDir, "createEmptyLexicon2.db")
+	lexName := "lex_test1"
+	ssName := "ss_test1"
+	locale := "en_US"
+	lexRefX := lex.NewLexRefWithInfo(dbPath, lexName, ssName)
+	closeAfter := false
+	createDB := true // if not exists
+
+	// 2. CREATE A NEW LEXICON IN A NEW DB
+	err = createEmptyLexicon(dbPath, lexRefX, locale, createDB, closeAfter)
+	if err != nil {
+		t.Errorf("didn't expect error here, found : %v", err)
+		return
+	}
+
+	// 3. REMOVE shm/wal FILES if they exist (they should not exist if the db has been properly closed)
+	walFile := dbPath + "-wal"
+	shmFile := dbPath + "-shm"
+	err = os.RemoveAll(walFile)
+	if err != nil {
+		t.Errorf("didn't expect error here, found : %v", err)
+		return
+	}
+	err = os.RemoveAll(shmFile)
+	if err != nil {
+		t.Errorf("didn't expect error here, found : %v", err)
+		return
+	}
+
+	// 4. OPEN THE SAME DB
+	lexRefX = lex.NewLexRefWithInfo(dbPath, lexName+"-2", ssName)
+	err = createEmptyLexicon(dbPath, lexRefX, locale, createDB, closeAfter)
+	if err == nil {
+		t.Errorf("expected error here, found : %v", err)
+		return
+	}
+}
+
+func init() {
+	dbapi.Sqlite3WithRegex()
+}
