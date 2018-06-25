c4b54adfb56e3d6f6a4f89582cee0d45d2ffec51
Hanna Lindgren <han.lind@gmail.com>
2018-06-25 12:28:25 +0200

    build_and_test.sh

--- build_and_test.sh
+++ build_and_test.sh
@@ -10,20 +10,20 @@ echo $basedir
 cd $basedir
 mkdir -p .build
 
-# go test -v ./... 
+go test -v ./... 
 
-# mkdir -p .build/appdir
+mkdir -p .build/appdir
 
-# bash install/setup.sh .build/appdir
+bash install/setup.sh .build/appdir
 
-# for proc in `ps --sort pid -Af|egrep pronlex| egrep -v  "grep .E"|sed 's/  */\t/g'|cut -f2`; do
-#     kill $proc
-# done
+for proc in `ps --sort pid -Af|egrep pronlex| egrep -v  "grep .E"|sed 's/  */\t/g'|cut -f2`; do
+    kill $proc
+done
 
-# bash install/start_server.sh -a .build/appdir &
-# export pid=$!
-# echo "pronlex server running on pid $pid. wait for $SLEEP seconds before shutting down"
-# sleep $SLEEP
-# sh .travis/exit_server_and_fail_if_not_running.sh pronlex $pid
+bash install/start_server.sh -a .build/appdir &
+export pid=$!
+echo "pronlex server running on pid $pid. wait for $SLEEP seconds before shutting down"
+sleep $SLEEP
+sh .travis/exit_server_and_fail_if_not_running.sh pronlex $pid
  
 docker build . --no-cache -t sttsse/pronlex:travis
