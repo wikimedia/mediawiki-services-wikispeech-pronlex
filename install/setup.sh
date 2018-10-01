1c472ffeda98d60840a2834aed6dcc97c696dd96
Hanna Lindgren <han.lind@gmail.com>
2018-10-01 16:37:28 +0200

    install/setup.sh added switch to run go binaries

--- install/setup.sh
+++ install/setup.sh
@@ -4,10 +4,17 @@ CMD=`basename $0`
 SCRIPTDIR=`dirname $0`
 export GOPATH=`go env GOPATH`
 export PATH=$PATH:$GOPATH/bin
+gobinaries=0
 
+if [ $# -eq 2 ] && [ $1 == "-b" ]; then
+    gobinaries=1
+    shift
+fi
 
 if [ $# -ne 1 ]; then
-    echo "USAGE: bash $CMD <APPDIR>
+    echo "USAGE: bash $CMD [options] <APPDIR>
+
+OPTIONS: -b go binaries (optional)
 
 Setup files will be added to the destination folder APPDIR.
 " >&2
@@ -23,6 +30,7 @@ fi
 LEXDB=wikispeech_testdb.db
 
 DEMOFILES=$GOPATH/src/github.com/stts-se/pronlex/lexserver/demo_files
+CMDDIR="$GOPATH/src/github.com/stts-se/pronlex/cmd/lexio"
 
 
 if [ -z "$GOPATH" ] ; then
@@ -31,6 +39,8 @@ if [ -z "$GOPATH" ] ; then
 fi
 
 function initial_setup {
+    echo "[$CMD] Go binaries: $gobinaries" >&2
+
     if [ -f $APPDIR/db_files/$LEXDB ] ; then
 	echo "[$CMD] Folder $APPDIR is already configured. No setup needed." >&2
 	exit 0
@@ -62,6 +72,17 @@ function initial_setup {
     cp $GOPATH/src/github.com/stts-se/pronlex/install/start_server.sh $APPDIR || exit 1
 }
 
+function run_go_command {
+    cmd=$1
+    args=${@:2}
+    if [ $gobinaries -eq 1 ]; then
+	$cmd $args
+    else
+	#echo "go run $CMDDIR/$cmd/$cmd.go $args"
+	go run $CMDDIR/$cmd/$cmd.go $args
+    fi
+}
+
 ### INITIAL SETUP
 initial_setup
 
@@ -71,7 +92,9 @@ initial_setup
 # install/.. is the root folder
 cd $SCRIPTDIR/..
 
-if go run cmd/lexio/createEmptyDB/createEmptyDB.go $APPDIR/db_files/$LEXDB ; then
+
+#if go run cmd/lexio/createEmptyDB/createEmptyDB.go $APPDIR/db_files/$LEXDB ; then
+if run_go_command createEmptyDB $APPDIR/db_files/$LEXDB ; then
     echo "[$CMD] Created empty db: $APPDIR/db_files/$LEXDB" >&2
 else
     echo "[$CMD] couldn't create empty db: $APPDIR/db_files/$LEXDB" >&2
@@ -85,9 +108,8 @@ fi
 echo "" >&2
 echo "IMPORT: $LEXDB:sv" >&2
 
-CMDDIR="$GOPATH/src/github.com/stts-se/pronlex/cmd/lexio"
-
-if go run $CMDDIR/importLex/importLex.go $APPDIR/db_files/$LEXDB sv sv_SE $DEMOFILES/swe030224NST.pron-ws.utf8.for_testing $APPDIR/symbol_sets/sv-se_ws-sampa.sym ; then
+#if go run $CMDDIR/importLex/importLex.go $APPDIR/db_files/$LEXDB sv sv_SE $DEMOFILES/swe030224NST.pron-ws.utf8.for_testing $APPDIR/symbol_sets/sv-se_ws-sampa.sym ; then
+if run_go_command importLex $APPDIR/db_files/$LEXDB sv sv_SE $DEMOFILES/swe030224NST.pron-ws.utf8.for_testing $APPDIR/symbol_sets/sv-se_ws-sampa.sym ; then
     echo -n ""
 else
     echo "$LEXDB:sv FAILED" >&2
@@ -96,7 +118,8 @@ fi
 
 echo "" >&2
 echo "IMPORT: $LEXDB:nb" >&2
-if go run $CMDDIR/importLex/importLex.go $APPDIR/db_files/$LEXDB nb nb_NO $DEMOFILES/nor030224NST.pron-ws.utf8.for_testing $APPDIR/symbol_sets/nb-no_ws-sampa.sym ; then
+#if go run $CMDDIR/importLex/importLex.go $APPDIR/db_files/$LEXDB nb nb_NO $DEMOFILES/nor030224NST.pron-ws.utf8.for_testing $APPDIR/symbol_sets/nb-no_ws-sampa.sym ; then
+if run_go_command importLex $APPDIR/db_files/$LEXDB nb nb_NO $DEMOFILES/nor030224NST.pron-ws.utf8.for_testing $APPDIR/symbol_sets/nb-no_ws-sampa.sym ; then
     echo -n ""
 else
     echo "$LEXDB:nb FAILED" >&2
@@ -105,7 +128,8 @@ fi
 
 echo "" >&2
 echo "IMPORT: $LEXDB:enu" >&2
-if go run $CMDDIR/importLex/importLex.go $APPDIR/db_files/$LEXDB enu en_US $DEMOFILES/cmudict-0.7b-ws.utf8.for_testing $APPDIR/symbol_sets/en-us_ws-sampa.sym ; then
+#if run_go_command importLex $APPDIR/db_files/$LEXDB enu en_US $DEMOFILES/cmudict-0.7b-ws.utf8.for_testing $APPDIR/symbol_sets/en-us_ws-sampa.sym ; then
+if run_go_command importLex $APPDIR/db_files/$LEXDB enu en_US $DEMOFILES/cmudict-0.7b-ws.utf8.for_testing $APPDIR/symbol_sets/en-us_ws-sampa.sym ; then
     echo -n ""
 else
     echo "$LEXDB:enu FAILED" >&2
@@ -114,7 +138,8 @@ else
 
 echo "" >&2
 echo "IMPORT: $LEXDB:ar" >&2
-if  go run $CMDDIR/importLex/importLex.go $APPDIR/db_files/$LEXDB ar ar_AR $DEMOFILES/ar_TEST.pron-ws.utf8.for_testing $APPDIR/symbol_sets/ar_ws-sampa.sym ; then
+#if  run_go_command importLex $APPDIR/db_files/$LEXDB ar ar_AR $DEMOFILES/ar_TEST.pron-ws.utf8.for_testing $APPDIR/symbol_sets/ar_ws-sampa.sym ; then
+if  run_go_command importLex $APPDIR/db_files/$LEXDB ar ar_AR $DEMOFILES/ar_TEST.pron-ws.utf8.for_testing $APPDIR/symbol_sets/ar_ws-sampa.sym ; then
     echo -n ""
 else
     echo "$LEXDB:ar FAILED" >&2
