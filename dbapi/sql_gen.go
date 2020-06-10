10430c720ac5d4453988d9ac7e97a29e44799bf3
Hanna Lindgren <han.lind@gmail.com>
2020-06-10 11:45:00 +0200

    bug fix in lexserver homograph search

--- dbapi/sql_gen.go
+++ dbapi/sql_gen.go
@@ -262,20 +262,20 @@ func users(q Query) (string, []interface{}) {
 
 func entryTag(q Query) (string, []interface{}) {
 
-	var res string
+	var res []string
 	var resv []interface{}
 
 	if q.MultipleTags {
-		res += " Entry.strn in (select distinct Entry.strn from EntryTag, Entry where EntryTag.entryId = Entry.id group by EntryTag.wordForm having count(EntryTag.wordForm)> 1)"
+		res = append(res, " Entry.strn in (select distinct Entry.strn from EntryTag, Entry where EntryTag.entryId = Entry.id group by EntryTag.wordForm having count(EntryTag.wordForm)> 1)")
 		//resv = append(resv, q.MultipleTags)
 	}
 
 	if q.TagLike != "" {
-		res += " Entry.id = EntryTag.entryId AND EntryTag.tag like ? "
+		res = append(res, " Entry.id = EntryTag.entryId AND EntryTag.tag like ? ")
 		resv = append(resv, q.TagLike)
 	}
 
-	return res, resv
+	return strings.Join(res, " AND "), resv
 }
 
 func comments(q Query) (string, []interface{}) {
